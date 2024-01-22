18e78c4d3b9a09936f9529933e5b2274c36f5ced
orkun.kozan <orkun.kozan@kgteknoloji.com>
2024-01-22 09:48:04 +0300

    birinci versiyon işlemler gönderilyior

--- TestApp/Program.cs
+++ TestApp/Program.cs
@@ -1,2 +1,10 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using TestApp.VersionsClasss;
+
 Console.WriteLine("Hello, World!");
+
+var versiyon1 = Versiyon1.Version1;
+
+Console.WriteLine(versiyon1);
+
+Console.ReadLine();
